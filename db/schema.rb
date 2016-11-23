40b097292a1bfcf75419e94e0d8f0cc8866757ee
Katrina Tollin <ktollin@gmail.com>
2016-11-23 17:26:37 -0600

    add paperclip gem, image form data

---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161123221309) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "meals", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "cuisine"
+    t.integer  "chef_id"
+    t.decimal  "price"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "patron_id"
+    t.integer  "meal_id"
+    t.integer  "quantity"
+    t.decimal  "total_price"
+    t.string   "destination_address"
+    t.datetime "delivery_time"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
+    t.string   "type"
+    t.string   "bio"
+    t.string   "street_address"
+    t.integer  "zip_code"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+end
