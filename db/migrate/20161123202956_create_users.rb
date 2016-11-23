525e0ccee8ae975184c955fa8006dcec9e75bc5e
Juan Martin Marco <jmmarco@gmail.com>
2016-11-23 15:27:50 -0600

    Add user migration

--- db/migrate/20161123202956_create_users.rb
+++ db/migrate/20161123202956_create_users.rb
@@ -1,8 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.string :oauth_token
+      t.string :oauth_secret
+      t.string :type
+      t.string :bio
+      t.string :street_address
+      t.integer :zip_code
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
