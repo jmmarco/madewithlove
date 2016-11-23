fe2546678f21f7f82f0aaf0dd29a47549fd9d09f
Juan Martin Marco <jmmarco@gmail.com>
2016-11-23 15:06:25 -0600

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :orders, foreign_key: :patron_id
   has_many :purchased_meals, through: :orders, source: :meals
   has_many :sales, through: :prepared_meals, source: :orders
+  has_many :supported_chef, through: :purchased_meals, source: :chef
+
+  validates :first_name, :last_name, :email, :password, :type, presence: true
 end
