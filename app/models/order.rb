99827d195b451b276a60b4bb14220d9c8e93d88f
Hoa Huynh <hoah@hotmail.com>
2016-11-23 19:35:41 -0600

    corrected associations and tested

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  belongs_to :users, foreign_key: 'patron_id'
-  belongs_to :meals
+  belongs_to :user, foreign_key: :patron_id, class_name: 'User'
+  belongs_to :meal, foreign_key: :meal_id, class_name: 'Meal'
 
   validates :quantity, :total_price, :destination_address, :delivery_time, presence: true
 end
