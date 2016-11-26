0bdb9cb6d9e37d9138c2a33fd44647cfb798cac2
Juan Martin Marco <jmmarco@gmail.com>
2016-11-25 21:27:35 -0600

    Include fontawesome

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
   // Add custom JS here
+
+  // Popover fun!
+  // Target the element
+  $("#profile img").popover({
+    placement: 'bottom',
+    container: 'body',
+    html: true,
+    trigger: "manual",
+    content: function() {
+      return $("#profile-popover").html();
+    }
+  // Show the popover when the the user hovers over the avatar
+  }).on("mouseenter", function() {
+    var _this = this;
+    $(this).popover("show");
+    $(this).siblings(".popover").on("mouseleave", function() {
+      $(_this).popover('hide');
+    });
+  // Hide the popover when the user leaves the popover menu
+  }).on("mouseleave", function() {
+    var _this = this;
+    setTimeout(function() {
+      if (!$(".popover:hover").length) {
+        $(_this).popover("hide");
+      }
+    }, 100);
+  });
+
+  
+
 });
