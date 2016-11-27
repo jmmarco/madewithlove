2a5b144a4efb9ca17e8e0ee7ad410a2f0d825ce0
Katrina Tollin <ktollin@gmail.com>
2016-11-27 12:25:37 -0600

    delivery time must be within four weeks

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,15 +4,19 @@ class Order < ApplicationRecord
 
   validates :quantity, :total_price, :destination_address, :delivery_time, presence: true
 
-  validate :delivery_time_cant_be_in_the_past, :delivery_time_must_be_at_least_a_day_in_advance
+  validate :delivery_time_cant_be_in_the_past, :delivery_time_must_be_at_least_a_day_in_advance, :delivery_time_must_be_no_more_than_four_weeks_in_advance
 
   def delivery_time_cant_be_in_the_past
     errors.add(:delivery_time, "can't be in the past") if
-      !delivery_time.blank? and delivery_time < Time.now
+      !delivery_time.blank? and delivery_time < DateTime.now
   end
 
   def delivery_time_must_be_at_least_a_day_in_advance
     errors.add(:delivery_time, "must be at least a day in advance") if
-      !delivery_time.blank? and Time.now < delivery_time - 1
+      !delivery_time.blank? and DateTime.now > delivery_time - 1.days
+  end
+
+  def delivery_time_must_be_no_more_than_four_weeks_in_advance
+    errors.add(:delivery_time, "must be no more than four weeks in advance") if !delivery_time? and DateTime.now + 28.days > delivery_time
   end
 end
