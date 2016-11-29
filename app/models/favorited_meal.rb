90c4c94283f6741397a80f8734cb064e56245146
Katrina Tollin <ktollin@gmail.com>
2016-11-29 12:22:48 -0600

    add validations for reviews, favorited_meals

--- app/models/favorited_meal.rb
+++ app/models/favorited_meal.rb
@@ -1,4 +1,7 @@
 class FavoritedMeal < ApplicationRecord
   belongs_to :user
   belongs_to :meal
+
+  validates :user_id, :meal_id, presence: true
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :order
   has_one :meal, through: :orders
   has_one :user, through: :orders
+
+  validates :order_id, presence: true
+
 end
