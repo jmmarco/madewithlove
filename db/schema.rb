650e204c864cccd4b76e34536612ca0837187919
Juan Martin Marco <jmmarco@gmail.com>
2016-11-30 11:49:25 -0600

    Add support for fonts on production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129225036) do
+ActiveRecord::Schema.define(version: 20161130054324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20161129225036) do
     t.string   "updated_image_content_type"
     t.integer  "updated_image_file_size"
     t.datetime "updated_image_updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
