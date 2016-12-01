e11940dc1a6dbfa268dad76e00a55ba18140e070
Katrina Tollin <ktollin@gmail.com>
2016-12-01 11:26:27 -0600

    add status column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129225036) do
+ActiveRecord::Schema.define(version: 20161201172500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,9 @@ ActiveRecord::Schema.define(version: 20161129225036) do
     t.decimal  "total_price"
     t.string   "destination_address"
     t.datetime "delivery_time"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.string   "status",              default: "pending"
   end
 
   create_table "reviews", force: :cascade do |t|
