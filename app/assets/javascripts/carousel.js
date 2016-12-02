15850fabb539a275cc79afd82a548816d8bf970f
Juan Martin Marco <jmmarco@gmail.com>
2016-12-01 19:18:00 -0600

    add hero js

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,4 +1,5 @@
-$(document).on('turbolinks:load', function() {
+$(document).ready(function() {
+
   $('.carousel').flickity({
     // options
     wrapAround: true,
@@ -7,8 +8,6 @@ $(document).on('turbolinks:load', function() {
     pauseAutoPlayOnHover: true,
     prevNextButtons: false,
     setGallerySize: false,
-
-
   });
 
 
@@ -17,33 +16,6 @@ $(document).on('turbolinks:load', function() {
     autoPlay: false,
     imagesLoaded: true,
   });
-
-
-  var images = [
-  "images/front/cooking_1.jpg",
-  "images/front/cooking_2.jpg",
-  "images/front/cooking_3.jpg",
-  "images/front/cooking_4.jpg",
-  "images/front/cooking_5.jpg",
-  "images/front/cooking_6.jpg",
-  "images/front/cooking_7.jpg",
-  ];
-
-  var heroText = [
-    "Homemade<br>meals<br>made for you!",
-    "Order  homemade<br>meals near you..",
-    "Delicious<br>comfort<br>food at<br>your fingertips",
-    "Good and<br>healthy<br>meals for you! "
-  ];
-
-  var imagesLength = images.length;
-  var heroTextLength = heroText.length;
-  var x = Math.floor(imagesLength * Math.random());
-  var y = Math.floor(heroTextLength * Math.random());
-
-  var a = images[0];
-  document.getElementById('image').src = images[x];
-  document.getElementById("hero").innerHTML = heroText[y];
 
 
 });
